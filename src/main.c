6cce5464d40e3befe001ff81056743f49a64d3b9
Deniz Karhan <81527587+denizkarhan@users.noreply.github.com>
2022-12-18 15:07:51 +0300

    Add files via upload
    Map içerisinde 1 birim hareket ederken engellerden geçmemesini ekledim, kesirli birimlerle hareket ederse engele giriyor orayı düzeltmeliyiz. Tavan için .xpm hava eklemeye çalıştım beceremedim onun yerine pixel bastırdım.

--- src/main.c
+++ src/main.c
@@ -82,7 +82,7 @@ void	process_args(t_game *game, char *path, t_player *player)
 	}
 }
 
-void	draw_square(t_img *img, int x, int y, int colour)
+void	draw_square(t_img *img, double x, double y, int colour)
 {
 	int	i;
 	int	j;
@@ -100,8 +100,9 @@ void	draw_square(t_img *img, int x, int y, int colour)
 	}
 }
 
-void	draw_minimap(t_img *img, t_game *game, t_player *player)
+void	draw_minimap(t_img *img, t_game *game, t_player *player, t_mlx *mlx)
 {
+	// printf("x: %f	y: %f\n", mlx->player.x, mlx->player.y); // -> Kameranın konumu
 	for (int i = 0; game->grid[i]; i++)
 	{
 		for (int j = 0; game->grid[i][j]; j++)
@@ -113,29 +114,80 @@ void	draw_minimap(t_img *img, t_game *game, t_player *player)
 		}
 	}
 	draw_square(img, player->x, player->y, 0x0000FF00);
+	mlx_put_image_to_window(mlx->mlx, mlx->window, img->img, 0, 0);
+}
+
+void	draw_map(t_img *img, t_game *game, t_player *player, t_mlx *mlx)
+{
+	int	median = HEIGHT / 2, y = -1, x;
+
+	while (++y < median)
+	{
+		x = -1;
+		while (++x < WIDTH)
+			my_mlx_pixel_put(img, x, y, 0XAB4F94CD);
+	}	
+	while (++y < HEIGHT)
+	{
+		x = -1;
+		while (++x < WIDTH)
+			my_mlx_pixel_put(img, x, y, 0XAB4BA123);
+	}
+}
+
+int	close_exit(t_mlx *vars)
+{
+	printf("Game Over!\n");
+	if (vars)
+		mlx_destroy_window(vars->mlx, vars->window);
+	exit(0);
+}
+
+int	keyhandler(int keycode, t_mlx *mlx)
+{
+	// Hareket olması için engel kontrolünü sağlayan koşullar eklendi (floating hareketlerde duvarlar bug da kalıyor)
+	if (keycode == 53)
+		exit(0);
+	if (keycode == 0)
+		{if (mlx->game.grid[(int)(mlx->player.y)][(int)(mlx->player.x - 1)] == '0')
+			mlx->player.x -= 1;}
+	else if (keycode == 2)
+		{if (mlx->game.grid[(int)(mlx->player.y)][(int)(mlx->player.x + 1)] == '0')
+			mlx->player.x += 1;}
+	else if (keycode == 1)
+		{if (mlx->game.grid[(int)(mlx->player.y + 1)][(int)(mlx->player.x)] == '0')
+			mlx->player.y += 1;}
+	else if (keycode == 13)
+		{if (mlx->game.grid[(int)(mlx->player.y - 1)][(int)(mlx->player.x)] == '0')
+			mlx->player.y -= 1;}
+	draw_minimap(&mlx->image, &mlx->game, &mlx->player, mlx);
+	return (0);
 }
 
 int	main(int ac, char **av)
 {
 	t_mlx		mlx;
-	t_img		image;
-	t_game		game;
-	t_player	player;
 
 	if (ac != 2)
 	{
 		perror("This program needs 2 arguments to function!");
 		exit(1);
 	}
-	process_args(&game, av[1], &player);
+	process_args(&mlx.game, av[1], &mlx.player);
 	mlx.mlx = mlx_init();
+
 	mlx.window = mlx_new_window(mlx.mlx, WIDTH, HEIGHT, "Cub3D");
-	image.img = mlx_new_image(mlx.mlx, WIDTH, HEIGHT);
-	image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian);
-	draw_minimap(&image, &game, &player);
-	mlx_put_image_to_window(mlx.mlx, mlx.window, image.img, 0, 0);
+	mlx.image.img = mlx_new_image(mlx.mlx, WIDTH, HEIGHT);
+	mlx.image.addr = mlx_get_data_addr(mlx.image.img, &mlx.image.bits_per_pixel, &mlx.image.line_length, &mlx.image.endian);
+	
+	draw_map(&mlx.image, &mlx.game, &mlx.player, &mlx); // Tavan ve zemini ekrana bastırıyor
+	draw_minimap(&mlx.image, &mlx.game, &mlx.player, &mlx);
+
+	mlx_hook(mlx.window, 2, 1L << 0, keyhandler, &mlx); // player hareketi -------> burada &mlx.player yolladığımız için x y 0 geliyordu.
+	mlx_hook(mlx.window, 17, 0, close_exit, &mlx); // esc
+
 	mlx_loop(mlx.mlx);
-	mlx_destroy_image(mlx.mlx, image.img);
+	mlx_destroy_image(mlx.mlx, mlx.image.img);
 	mlx_destroy_window(mlx.mlx, mlx.window);
 	exit(0);
 }
